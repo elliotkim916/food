711c894fbdbf35a8824b94d94e448556a350c82e
Elliot Kim <elliotkim916@gmail.com>
2020-03-28 17:39:49 -0700

    Added api calls, try catch block, and error handling

---
+++ src/api/yelp.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://api.yelp.com/v3/businesses',
+  headers: {
+    Authorization: 
+      'BeareraVqqO_5awM7YjGV-kH5_q6gWNqbZjJPkgCVvmx4s-MmP6MiC3IcII_CYPO9i3fOil0rRh2vDQ3tNuYnk_niNny_WYP1OYgnTV7LdU9pTps7ASvNAhB6BwQwQkpJ_XnYx'
+  }
+});
